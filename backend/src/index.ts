b0ead8000abcf7b0b0e48ba387a91e09ccf27b5b
AkromAbdaliev <jacquesalaiyxs@gmail.com>
2024-03-09 17:00:03 +0200

    massive features added input btn and fetching data and showing it

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import * as RecipeAPI from './recipe-api';
+import { constants } from 'buffer';
 
 const app = express();
 
@@ -15,6 +16,12 @@ app.get('/api/recipes/search', async (req, res) => {
   const results = await RecipeAPI.searchRecipes(searchTerm, page); // assigned to results
 
   return res.json(results); //returned all info in json
+});
+
+app.get('/api/recipes/:recipeId/summary', async (req, res) => {
+  const recipeId = req.params.recipeId;
+  const results = await RecipeAPI.getRecipeSummary(recipeId);
+  return res.json(results);
 });
 
 app.listen(5000, () => {
